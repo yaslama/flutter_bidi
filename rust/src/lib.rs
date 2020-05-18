f61a5d7a200c5d85c0fbd963e695698d2f18e2ec
Yaacov Akiba Slama <ya@slamail.org>
2020-05-18 16:33:41 +0300

    Reorder the full string

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,27 +1,35 @@
-use std::os::raw::{c_char};
-use std::ffi::{CString, CStr};
-use unic_bidi::{BidiInfo, level};
+use std::borrow::Cow;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+use unic_bidi::{level, BidiInfo};
+
+fn reorder_paras(text: &str) -> Vec<Cow<'_, str>> {
+    let bidi_info = BidiInfo::new(text, Some(level::RTL_LEVEL));
+    bidi_info
+        .paragraphs
+        .iter()
+        .map(|para| bidi_info.reorder_line(para, para.range.clone()))
+        .collect()
+}
 
 #[no_mangle]
-pub extern fn reorder(to: *const c_char) -> *mut c_char {
+pub extern "C" fn reorder(to: *const c_char) -> *mut c_char {
     let c_str = unsafe { CStr::from_ptr(to) };
     let text = match c_str.to_str() {
         Err(_) => "",
         Ok(string) => string,
     };
 
-    let bidi_info = BidiInfo::new(&text, Some(level::RTL_LEVEL));
-    let para = &bidi_info.paragraphs[0];
-    let line = para.range.clone();
-    let display = bidi_info.reorder_line(para, line);
-
+    let display = reorder_paras(text).join("\n");
     CString::new(display.to_string()).unwrap().into_raw()
 }
 
 #[no_mangle]
-pub extern fn rust_cstr_free(s: *mut c_char) {
+pub extern "C" fn rust_cstr_free(s: *mut c_char) {
     unsafe {
-        if s.is_null() { return }
+        if s.is_null() {
+            return;
+        }
         CString::from_raw(s)
     };
 }
